c1080fdd4b95faf010a472d0f9495c9ec6db56eb
Mohammed Omar <omartechmohammed@gmail.com>
2024-03-05 23:24:32 -0500

    started the coding challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -7,6 +7,17 @@ class Article:
 class Author:
     def __init__(self, name):
         self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self. name):
+        if isinstance(name. str) and len(name) > 0:
+            self._name = name
+        else: 
+            raise Exception
 
     def articles(self):
         pass
